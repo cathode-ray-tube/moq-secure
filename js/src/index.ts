76b50bb30d35faa9be323ad4c6dae967e2e8027f
cathode-ray-tube <cathode-ray222@proton.me>
2026-08-25 23:54:04 +0100

    Re-export modules from multiple files

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,1 +1,8 @@
-export {};
+export * from "./constants.js";
+export * from "./errors.js";
+export * from "./nonce.js";
+export * from "./padding.js";
+export * from "./keys.js";
+export * from "./crypto.js";
+export * from "./wire.js";
+export * from "./streams.js";
