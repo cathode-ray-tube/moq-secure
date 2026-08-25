d408549fafe4c57a88ba730679609ae517715eca
cathode-ray-tube <cathode-ray222@proton.me>
2026-08-25 23:49:08 +0100

    Add constants for MAGIC, VERSION, and lengths

---
+++ js/src/constants.ts
@@ -0,0 +1,6 @@
+export const MAGIC = new Uint8Array([0x4d, 0x4f, 0x51, 0x53]); // MOQS
+export const VERSION = 1;
+
+export const SIG_SLOT_LEN = 64;
+export const AEAD_TAG_LEN = 16;
+export const FIXED_HEADER_LEN = 28;
