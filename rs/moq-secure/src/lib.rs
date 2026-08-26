d6ac2bd5e4f96a7121c90491f1965e745a2b678a
cathode-ray-tube <cathode-ray222@proton.me>
2026-08-26 20:15:39 +0100

    Re-export derive_nonce12 from nonce module

--- rs/moq-secure/src/lib.rs
+++ rs/moq-secure/src/lib.rs
@@ -9,4 +9,6 @@ pub use error::MoqSecureError;
 // Re-export key store types so your app can construct/populate it.
 pub use key_store::{InMemoryKeyStore, KeyStore, KeyStoreError};
 
+pub use nonce::derive_nonce12;
+
 pub use wire::{decrypt_frame, encrypt_frame, Frame, WireHeader, MAGIC, VERSION};
