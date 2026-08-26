475cd35d6eb9a6cfdda1d63ea44f6b0681ca9936
cathode-ray-tube <cathode-ray222@proton.me>
2026-08-26 22:33:25 +0100

    Remove AEAD_TAG_LEN export from crypto.ts
    Removed the export of AEAD_TAG_LEN constant.

--- js/src/crypto.ts
+++ js/src/crypto.ts
@@ -4,8 +4,6 @@ import { sha256 } from "@noble/hashes/sha2.js";
 import { deriveNonce12 } from "./nonce.js";
 import { MoqSecureError } from "./errors.js";
 
-export const AEAD_TAG_LEN = 16;
-
 export function sha256Digest(data: Uint8Array): Uint8Array {
   return sha256(data);
 }
