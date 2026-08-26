7f5b99f1406f0885bde2be8453c2297c116d7bc1
cathode-ray-tube <cathode-ray222@proton.me>
2026-08-26 20:12:15 +0100

    Add VectorFile and related structs for deserialization

---
+++ test/rs/common/mod.rs
@@ -0,0 +1,42 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct VectorFile {
+    pub version: u8,
+    #[serde(rename = "aeadKey")]
+    pub aead_key: String,
+    #[serde(rename = "ed25519Seed")]
+    pub ed25519_seed: String,
+    #[serde(rename = "nonceVectors")]
+    pub nonce_vectors: Vec<NonceVector>,
+    pub frames: Vec<FrameVector>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct NonceVector {
+    #[serde(rename = "keyId")]
+    pub key_id: u8,
+    pub ctr: String,
+    pub nonce: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct FrameVector {
+    pub name: String,
+    pub plaintext: String,
+    pub frame: String,
+    pub header: String,
+    pub payload: String,
+    pub tag: String,
+    pub signature: Option<String>,
+    pub lease: u8,
+}
+
+pub fn read_vectors() -> VectorFile {
+    serde_json::from_str(include_str!("../test-vectors/frames.json"))
+        .expect("valid frames.json")
+}
+
+pub fn hex_decode(value: &str) -> Vec<u8> {
+    hex::decode(value).expect("valid hex")
+}
