3bc4102d5027401a6c971d726bc64b5a5e31829d
cathode-ray-tube <cathode-ray222@proton.me>
2026-09-15 14:32:24 +0100

    Fix NONCE_PREFIX_3 array type and update usage

--- rs/moq-secure/src/nonce.rs
+++ rs/moq-secure/src/nonce.rs
@@ -1,9 +1,9 @@
-pub const NONCE_PREFIX_3: [u8] = *b"non";
+pub const NONCE_PREFIX_3: [u8; 3] = *b"non";
 
 pub fn derive_nonce12(key_id: u8, ctr: u64) -> [u8; 12] {
 	let mut nonce = [0u8; 12];
 
-	nonce[..3].copy_from_slice(NONCE_PREFIX_3);
+	nonce[..3].copy_from_slice(&NONCE_PREFIX_3);
 	nonce[3] = key_id;
 	nonce[4..].copy_from_slice(&ctr.to_be_bytes());
 
