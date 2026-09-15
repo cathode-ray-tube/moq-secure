42be6762826528f33b0fa002158dab4a7530a50f
cathode-ray-tube <cathode-ray222@proton.me>
2026-09-15 17:48:04 +0100

    Refactor derive_nonce12 to simplify nonce creation
    Refactor derive_nonce12 function to use array initialization for nonce.

--- rs/moq-secure/src/nonce.rs
+++ rs/moq-secure/src/nonce.rs
@@ -1,11 +1,20 @@
 pub const NONCE_PREFIX_3: [u8; 3] = *b"non";
 
 pub fn derive_nonce12(key_id: u8, ctr: u64) -> [u8; 12] {
-	let mut nonce = [0u8; 12];
+    let ctr = ctr.to_be_bytes();
 
-	nonce[..3].copy_from_slice(&NONCE_PREFIX_3);
-	nonce[3] = key_id;
-	nonce[4..].copy_from_slice(&ctr.to_be_bytes());
-
-	nonce
+    [
+        NONCE_PREFIX_3[0],
+        NONCE_PREFIX_3[1],
+        NONCE_PREFIX_3[2],
+        key_id,
+        ctr[0],
+        ctr[1],
+        ctr[2],
+        ctr[3],
+        ctr[4],
+        ctr[5],
+        ctr[6],
+        ctr[7],
+    ]
 }
